0495c07443bf1171b8218d5011a6079153b0b081
Piotr Owerko <piotrowerko@gmail.com>
2025-03-20 11:37:39 +0100

    checking if rejection

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from main import greet
 
 class TestHello(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World2!", "Powitanie nie jest zgodne z oczekiwaniem")
+        self.assertEqual(greet("World"), "Hello, World2!", "Powitanie nie jest zgodne z oczekiwaniem!")
 
 if __name__ == '__main__':
     unittest.main()
